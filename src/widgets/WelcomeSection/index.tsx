619ccc247c719e93abce34efabae55f148a4cecd
webdiller <eugenefromrus@gmail.com>
2023-12-22 22:43:48 +1000

    feat: upd banners

--- src/widgets/WelcomeSection/index.tsx
+++ src/widgets/WelcomeSection/index.tsx
@@ -6,12 +6,86 @@ import { Pagination, Autoplay } from "swiper/modules"
 import imgBanner from "@/public/img-banner.jpg"
 import imgBannerLg from "@/public/img-banner-lg.jpg"
 
+import imgBanner1MobileJpg from "@/public/banners/banner-1-mob.jpg"
+import imgBanner1MobileWebp from "@/public/banners/banner-1-mob.webp"
+import imgBanner1DeskJpg from "@/public/banners/banner-1-desk.jpg"
+import imgBanner1DeskWebp from "@/public/banners/banner-1-desk.webp"
+
+import imgBanner2MobileJpg from "@/public/banners/banner-2-mob.jpg"
+import imgBanner2MobileWebp from "@/public/banners/banner-2-mob.webp"
+import imgBanner2DeskJpg from "@/public/banners/banner-2-desk.jpg"
+import imgBanner2DeskWebp from "@/public/banners/banner-2-desk.webp"
+
+import imgBanner3MobileJpg from "@/public/banners/banner-3-mob.jpg"
+import imgBanner3MobileWebp from "@/public/banners/banner-3-mob.webp"
+import imgBanner3DeskJpg from "@/public/banners/banner-3-desk.jpg"
+import imgBanner3DeskWebp from "@/public/banners/banner-3-desk.webp"
+
+import imgBanner4MobileJpg from "@/public/banners/banner-4-mob.jpg"
+import imgBanner4MobileWebp from "@/public/banners/banner-4-mob.webp"
+import imgBanner4DeskJpg from "@/public/banners/banner-4-desk.jpg"
+import imgBanner4DeskWebp from "@/public/banners/banner-4-desk.webp"
+import { StaticImageData } from "next/image"
+
 interface ComponentProps {}
+interface BannerProps {
+  mobile: {
+    webp: StaticImageData
+    jpg: StaticImageData
+  }
+  desktop: {
+    webp: StaticImageData
+    jpg: StaticImageData
+  }
+}
 
 const WelcomeSection: FC<ComponentProps> = () => {
   const [swiperLoaded, setSwiperLoaded] = useState(false)
   const swiperRef = useRef<SwiperRef>()
   const pagination = useRef(null)
+
+  const banners: BannerProps[] = [
+    {
+      desktop: {
+        jpg: imgBanner1DeskJpg,
+        webp: imgBanner1DeskWebp,
+      },
+      mobile: {
+        jpg: imgBanner1MobileJpg,
+        webp: imgBanner1MobileWebp,
+      },
+    },
+    {
+      desktop: {
+        jpg: imgBanner2DeskJpg,
+        webp: imgBanner2DeskWebp,
+      },
+      mobile: {
+        jpg: imgBanner2MobileJpg,
+        webp: imgBanner2MobileWebp,
+      },
+    },
+    {
+      desktop: {
+        jpg: imgBanner3DeskJpg,
+        webp: imgBanner3DeskWebp,
+      },
+      mobile: {
+        jpg: imgBanner3MobileJpg,
+        webp: imgBanner3MobileWebp,
+      },
+    },
+    {
+      desktop: {
+        jpg: imgBanner4DeskJpg,
+        webp: imgBanner4DeskWebp,
+      },
+      mobile: {
+        jpg: imgBanner4MobileJpg,
+        webp: imgBanner4MobileWebp,
+      },
+    },
+  ]
 
   return (
     <div className="relative">
@@ -22,7 +96,6 @@ const WelcomeSection: FC<ComponentProps> = () => {
           clickable: true,
           bulletElement: "button",
         }}
-       
         className="text-left"
         slidesOffsetBefore={35}
         spaceBetween={35}
@@ -46,24 +119,32 @@ const WelcomeSection: FC<ComponentProps> = () => {
             spaceBetween: 0,
           },
         }}>
-        {Array.from({ length: 5 }).map((item, indx) => {
+        {banners.map((item, indx) => {
           return (
             <SwiperSlide
               className="relative flex items-end justify-center bg-[#EFEFEF]"
               key={indx}>
               <picture className="w-full">
+                {/* LG */}
                 <source
                   media="(min-width: 576px)"
-                  srcSet={imgBannerLg.src}
+                  srcSet={item.desktop.webp.src}
+                />
+                <source
+                  media="(min-width: 576px)"
+                  srcSet={item.desktop.jpg.src}
+                />
+                <source
+                  srcSet={item.mobile.webp.src}
                 />
                 <img
                   className="h-auto w-full max-w-none object-contain transition-all duration-500"
                   alt="Изображение"
-                  src={imgBanner.src}
+                  src={item.mobile.jpg.src}
                 />
               </picture>
-              <div className="absolute bottom-3 flex justify-center lg:bottom-[unset] lg:left-10 lg:top-[220px] lg:pb-5 xl:left-16 xl:top-[350px]">
-                <SharedButton classNames="text-white w-auto">Оформить заказ</SharedButton>
+              <div className="absolute bottom-3 flex justify-center lg:left-[10vw] lg:bottom-[20px] lg:pb-5 xl:left-[14vw]">
+                <SharedButton classNames="text-white w-auto lg:min-h-[78px] lg:max-h-[78px] lg:px-[45px] lg:text-[30px]">Оформить заказ</SharedButton>
               </div>
             </SwiperSlide>
           )
